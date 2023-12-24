596bd852f3041b0857661ee06b05bdfd913e29c1
Sum idiot <61273547+hyuuwu@users.noreply.github.com>
2023-12-24 10:20:14 -0300

    Create maconha.py

---
+++ maconha.py
@@ -0,0 +1,12 @@
+import webbrowser as wb
+
+url = "github.com/hyuuwu"
+
+saas = input("imput ur new password")
+efg = input("insert it again")
+if efg == saas:
+    print("thanks,gay")
+    while True:
+        wb.open(url)
+else:
+    print("grr! >:(")
